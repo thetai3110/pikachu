0d6d8e67d793fae0af1a95deb4324c3696a67009
Tài Trần <38857214+thetai3110@users.noreply.github.com>
2021-07-24 16:35:36 +0700

    Config project #1
    - Add some folders
    - Config i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
+import i18n from './i18n/i18n';
+import { useEffect } from 'react';
 
 function App() {
+  const { t } = useTranslation();
+  const language = localStorage.getItem('language');
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+    localStorage.setItem('language', lng);
+  }
+  useEffect(()=>{
+    if(!localStorage.getItem('language')){
+      localStorage.setItem('language', 'vi');
+    }
+  },[])
+  useEffect(() => {
+    i18n.changeLanguage(language || 'vi');
+  }, [language])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +27,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+          <button onClick={() => changeLanguage('vi')}>Tiếng việt</button>
+          <button onClick={() => changeLanguage('en')}>English</button>
+
+          {t('name')} + {language}
       </header>
     </div>
   );
---
+++ src/i18n/i18n.ts
@@ -0,0 +1,26 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: {
+          "name": "Dylan"
+        },
+      },
+      vi: {
+        translation: {
+          "name": "Tài"
+        },
+      },
+    },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
